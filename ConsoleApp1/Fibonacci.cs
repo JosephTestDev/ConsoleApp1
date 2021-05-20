38d03d5f74c3d654f767061f46246fe70d6cad39
Joe Purcell <jpurcell@INVIA.local>
2021-05-20 14:02:26 -0400

    Add project files.

---
+++ ConsoleApp1/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Fibonacci
+    {
+        public static List<int> GetFibonacciSequence(int fibIndex)
+        {
+            List<int> sequence = new List<int>();
+            sequence.Add(1);
+            sequence.Add(1);
+            if (fibIndex > 2 )
+            {
+                sequence = recurse(fibIndex - 2, sequence);
+            }
+            return sequence;
+        }
+        private static List<int> recurse(int count, List<int> sequence)
+        {
+            if(count > 0)
+            {
+                sequence.Add(sequence[sequence.Count - 1] + sequence[sequence.Count - 2]);
+                count--;
+                return recurse(count, sequence);
+            }
+            return sequence;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> fib = Fibonacci.GetFibonacciSequence(5);
+            for(int index = 0; index < fib.Count; index++)
+                Console.WriteLine(fib[index]);
+        }
+    }
+}
